d98c262b585c9880da0c0e2f55b13a3a4eb7da32
diovalda <ferrenxx@gmail.com>
2025-05-14 09:17:21 +0700

    fix: host

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 8000,
+    host: "192.168.20.179",
+    port: 5173,
   },
 });
